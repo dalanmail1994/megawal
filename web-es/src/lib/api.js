02eecb295ce73c9a8b9a71d131acc325521773e2
dalanmail1994 <dalanmail1994@gmail.com>
2026-01-11 17:56:09 +0200

    update to right domain names

--- web-es/src/lib/api.js
+++ web-es/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API_BASE_URL = "http://127.0.0.1:8000/api";
 // const API_BASE_URL = "http://168.100.11.23/api";
-const API_BASE_URL = "https://api.winera.co/api";
+const API_BASE_URL = "https://api.greenbit.live/api";
 
 
 
